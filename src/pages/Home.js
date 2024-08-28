8189665336d495eb38fd725440ab5993557e4352
BahaaCaillou <tohami.bahaa93@gmail.com>
2024-08-28 12:01:36 +0200

    details home cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { auth } from '../auth/auth';
 import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button } from '@nextui-org/react';
 import { NavLink } from 'react-router-dom';
+import { TimeInput } from "@nextui-org/react";
+import { Time } from "@internationalized/date";
 
 const Home = () => {
   const [events, setEvents] = useState([]);
@@ -15,6 +17,7 @@ const Home = () => {
         const headers = auth();
         const response = await axios.get('http://localhost:9000/event/events', { headers });
         setEvents(response.data);
+        console.log('Événements récupérés avec succès:', response.data);
       } catch (error) {
         console.error('Erreur lors de la récupération des événements :', error);
       }
@@ -30,49 +33,64 @@ const Home = () => {
 
   // Changer de page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', minHeight: '100vh', backgroundColor: 'black' }}>
       <div style={{ width: '100%', maxWidth: '800px', margin: '1rem' }}>
         {currentEvents.length > 0 ? (
-          currentEvents.map(event => (
-            <Card key={event.event_id} className="max-w-[800px]" style={{ marginBottom: '2rem'}}>
-              <CardHeader className="flex gap-3">
-                <Image
-                  alt="event image"
-                  height={40}
-                  radius="sm"
-                  src="https://via.placeholder.com/40" // Remplacez par l'URL de l'image de l'événement si disponible
-                  width={40}
-                />
-                <div className="flex flex-col">
-                  <p className="text-md">{event.title}</p>
-                  <p className="text-small text-default-500">{event.location}</p>
-                </div>
-              </CardHeader>
-              <Divider />
-              <CardBody>
-                <p>{event.description}</p>
-                <p>Date: {event.date}</p>
-                <p>Heure: {event.time}</p>
-                <p>Organisateur: {event.created_by}</p> {/* Assurez-vous que le champ est correct */}
-                <p>Capacité maximale: {event.capacity}</p>
-                <NavLink href="">
-                En savoir plus
-                </NavLink>
-              </CardBody>
-              <Divider />
-              <CardFooter>
-                <Link
-                  isExternal
-                  showAnchorIcon
-                  href={`https://maps.google.com/?q=${event.location}`} // Lien vers la localisation de l'événement
-                >
-                  Voir la localisation
-                </Link>
-              </CardFooter>
-            </Card>
-          ))
+          currentEvents.map(event => {
+            const eventTime = new Time(event.time.split(':')[0], event.time.split(':')[1]);
+            return (
+              <Card key={event.event_id} className="max-w-[800px]" style={{ marginBottom: '2rem' }}>
+                <CardHeader className="flex gap-3">
+                  <Image
+                    alt="event image"
+                    height={40}
+                    radius="sm"
+                    src="https://via.placeholder.com/40" // Remplacez par l'URL de l'image de l'événement si disponible
+                    width={40}
+                  />
+                  <div className="flex flex-col">
+                    <h1 className="text-xl">{event.title}</h1>
+                    <p className="text-small text-default-500">{event.location}</p>
+                  </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                  <p className="text-lg">{event.description}</p>
+                  <p>
+                    {new Date(event.date).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })}
+                  </p>
+
+                  <br />
+                  <TimeInput
+                    isReadOnly
+                    label="Event Time"
+                    defaultValue={eventTime}
+                  />
+                  <br />
+                  <p>Organisé par {capitalizeFirstLetter(event.User.username)}</p> {/* Assurez-vous que le champ est correct */}
+                  <p>Capacité maximale de {event.capacity} personnes</p>
+                  <NavLink href="">
+                    En savoir plus
+                  </NavLink>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <Link
+                    isExternal
+                    showAnchorIcon
+                    href={`https://maps.google.com/?q=${event.location}`} // Lien vers la localisation de l'événement
+                  >
+                    Voir la localisation
+                  </Link>
+                </CardFooter>
+              </Card>
+            );
+          })
         ) : (
           <p>Aucun événement disponible.</p>
         )}
