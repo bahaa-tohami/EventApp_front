b2709c2ce4f5fe6d6f1e15c4cc4b5370dffa64d0
PIGNA Carole <carole.pigna@gmail.com>
2024-08-30 21:19:49 +0200

    Composant InviteUserForm rajouté , ajout de l'heure de l'venement, mise en page homogene (a peu près comme home)

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardHeader, CardBody, Input, Textarea, Accordion, AccordionItem, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from '@nextui-org/react';
+import { Card, CardHeader, CardBody, CardFooter, Input, Divider, Textarea, Accordion, AccordionItem, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Avatar, Rating } from '@nextui-org/react';
 import axios from 'axios';
 import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
 import { Image } from '@nextui-org/react';
 import { useParams } from 'react-router-dom';
-
+import { TimeInput } from "@nextui-org/react";
+import { Time } from "@internationalized/date";
+import InviteUserButton from '../components/InviteUserButton.js';
+import InviteUserForm from '../components/InviteUserForm.js';
+import { useAuth } from '../auth/AuthContext.js';
 // Mock functions for fetching event and user data
-const fetchEventById = async (eventId)=> {
+const fetchEventById = async (eventId) => {
     const user = JSON.parse(localStorage.getItem('user'));
 
 
@@ -33,6 +37,8 @@ const fetchEventById = async (eventId)=> {
 
 
 const EventDetails = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
     const { eventId } = useParams();
     const [event, setEvent] = useState(null);
     const [participantCount, setParticipantCount] = useState(0);
@@ -48,14 +54,20 @@ const EventDetails = () => {
 
     const { isOpen: isInvitesModalOpen, onOpen: onInvitesOpen, onClose: onInvitesClose } = useDisclosure();
     const { isOpen: isCommentModalOpen, onOpen: onCommentOpen, onClose: onCommentClose } = useDisclosure();
+     const { isOpen: isInvitationModalOpen, onOpen:onInvitatioOpen, onOpenChange } = useDisclosure();
+   
 
+   
+    
 
     const handleRating = (rate) => {
         setRatingValue(rate);
     };
 
     useEffect(() => {
+
         const loadEvent = async () => {
+
             try {
                 const eventData = await fetchEventById(eventId);
                 setEvent(eventData);
@@ -97,6 +109,7 @@ const EventDetails = () => {
 
     const handleInviteUser = () => {
         // Logic to handle inviting a user
+
         console.log(`User invited: ${invitedUser}`);
         setInvitedUser('');
     };
@@ -151,6 +164,33 @@ const EventDetails = () => {
             </div>
         );
     };
+
+    const RatingOneComment = ({ rating }) => {
+        const colors = {
+            orange: "#F2C265",
+            grey: "#a9a9a9"
+        };
+        const stars = Array(5).fill(0);
+
+        return (
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                {stars.map((_, index) => (
+                    <FaStar
+                        key={index}
+                        size={12}
+                        color={rating > index ? colors.orange : colors.grey}
+                    />
+                ))}
+                <p style={{ marginLeft: 8 }}>{rating}</p>
+            </div>
+        );
+    };
+
+    //affichage d'une note par commentaire
+
+
+
+
     // Fonction pour mettre à jour totalStars, s'assurant qu'il ne dépasse pas 5
     const updateTotalStars = (newTotalStars) => {
         if (newTotalStars <= 5) {
@@ -183,102 +223,147 @@ const EventDetails = () => {
         }
         return stars;
     };
-
-
+    const eventTime = new Time(event.time.split(':')[0], event.time.split(':')[1]);
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
 
     return (
-        <div className="flex justify-center items-center min-h-screen">
-            <div className="w-full max-w-md flex flex-col gap-6">
-                <Card className="pb-10">
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', minHeight: '100vh', backgroundColor: 'black' }}>
+            <div style={{ width: '100%', maxWidth: '800px', margin: '1rem' }}>
+                <Card className="max-w-[800px]" style={{ marginBottom: '2rem' }}>
                     <CardHeader className="flex gap-3">
-                        <div>
-                            <h1 className="text-2xl font-bold">Détails de l'événement</h1>
-                            <p className="text-sm text-default-500">Titre: {event.title}</p>
-                            <p className="text-sm text-default-500">Description: {event.description}</p>
-                            <p className="text-sm text-default-500">Date: {event.date}</p>
-                            <p className="text-sm text-default-500">Location: {event.location}</p>
-                            {currentUser && currentUser.userId === event.created_by && (
-                                <>
-                                    <div className="flex gap-4 items-center">
-                                        <p className="text-sm text-default-500">Nombre d'inscrits: {participantCount}</p>
-                                        <>
-                                            <Button onPress={onInvitesOpen}>Afficher la liste</Button>
-                                            <Modal isOpen={isInvitesModalOpen} onOpenChange={onInvitesClose}>
-                                                <ModalContent>
-                                                    {(onClose) => (
-                                                        <>
-                                                            <ModalHeader className="flex flex-col gap-1">Liste des invités</ModalHeader>
-                                                            <ModalBody>
-                                                                {event.Participants && event.Participants.length > 0 ? (
-                                                                    <ul>
-                                                                        {event.Participants.map((participant) => (
-                                                                            <li key={participant.user_id} className="border p-2 mb-2 rounded flex items-center gap-3">
-                                                                                <Image
-                                                                                    alt="user avatar"
-                                                                                    height={40}
-                                                                                    radius="sm"
-                                                                                    src="https://avatar.iran.liara.run/public"
-                                                                                    width={40}
-                                                                                />
-                                                                                <div className="flex flex-col">
-                                                                                    <p className="text-md">
-                                                                                      {participant.User.username || 'Utilisateur inconnu'}
-                                                                                    </p>
-                                                                                </div>
-                                                                            </li>
-                                                                        ))}
-                                                                    </ul>
-                                                                ) : (
-                                                                    <p>Aucun participant disponible.</p>
-                                                                )}
-                                                            </ModalBody>
-
-                                                            <ModalFooter>
-                                                                <Button color="danger" variant="light" onPress={onClose}>
-                                                                    Close
-                                                                </Button>
-
-                                                            </ModalFooter>
-                                                        </>
-                                                    )}
-
-                                                </ModalContent>
-                                            </Modal>
-                                        </>
-
-                                    </div>
-                                </>
-                            )}
-                            <div className="text-sm text-default-500">
-
-
-                                <Accordion>
-                                    <AccordionItem key="1" aria-label="Commentaires" title="Commentaires" subtitle="Cliquez pour lire les commentaires">
-                                        {Array.isArray(event.Comments) && event.Comments.length > 0 ? (
-                                            <ul>
-                                                {event.Comments.map((comment, index) => (
-                                                    <li key={index} className="border p-2 mb-2 rounded">
-                                                        <p><strong>Contenu:</strong> {comment.content || 'Pas de contenu'}</p>
-                                                        <p><strong>Évaluation:</strong> {comment.rating !== undefined ? comment.rating : 'Non défini'}</p>
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        ) : (
-                                            <p>Aucun commentaire disponible.</p>
-                                        )}
-                                    </AccordionItem>
-                                </Accordion>
-                            </div>
-                            <p><strong>Note moyenne:</strong></p>
-                            <div><RatingDisplay comments={event.Comments} /></div>
-
+                        <Image
+                            alt="event image"
+                            height={40}
+                            radius="sm"
+                            src="https://via.placeholder.com/40" // Remplacez par l'URL de l'image de l'événement si disponible
+                            width={40}
+                        />
+                        <div className="flex flex-col">
+                            <h1 className="text-xl">{event.title}</h1>
+                            <p className="text-small text-default-500">{event.location}</p>
                         </div>
                     </CardHeader>
-                    <CardBody className="flex flex-col gap-4">
+                    <Divider />
+                    <CardBody>
+
+                        <p className="text-sm text-default-500"><strong>Titre:</strong> {event.title}</p>
+                        <p className="text-sm text-default-500"><strong>Description:</strong> {event.description}</p>
+                        <p className="text-sm text-default-500">
+                            <strong>Date:</strong> {new Date(event.date).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })}
+
+                        </p>
+                        <p className="text-sm text-default-500"><strong>Lieu:</strong> {event.location}</p>
+                        <br />
+                        <TimeInput
+                            isReadOnly
+                            label="Event Time"
+                            defaultValue={eventTime}
+                        />
+                        <br />
+                        {/* <p>Organisé par {capitalizeFirstLetter(user.username)}</p>  */}{/* Make sure the field is correct */}
+                        <p style={{ paddingBottom: '20px' }} ><strong>Capacité maximale: </strong> {event.capacity} personnes</p>
+                        <div className='flex gap-4 items-center '>
+                            <p><strong>Note moyenne:</strong></p>
+                            <div><RatingDisplay comments={event.Comments} /></div>
+                        </div>
+
+                        <div>
+                            {currentUser && currentUser.userId === event.created_by && (
+
+
+                                <div className="flex gap-4 items-center ">
+
+                                    <p><strong>Nombre d'inscrits: </strong>{participantCount}</p>
+                                    <div>
+                                        <Button onPress={onInvitesOpen}>Afficher la liste</Button>
+                                        <Modal isOpen={isInvitesModalOpen} onOpenChange={onInvitesClose}>
+                                            <ModalContent>
+                                                {(onClose) => (
+                                                    <>
+                                                        <ModalHeader className="flex flex-col gap-1">Liste des invités</ModalHeader>
+                                                        <ModalBody>
+                                                            {event.Participants && event.Participants.length > 0 ? (
+                                                                <ul>
+                                                                    {event.Participants.map((participant) => (
+                                                                        <li key={participant.user_id} className="border p-2 mb-2 rounded flex items-center gap-3">
+                                                                            <Image
+                                                                                alt="user avatar"
+                                                                                height={40}
+                                                                                radius="sm"
+                                                                                src="https://avatar.iran.liara.run/public"
+                                                                                width={40}
+                                                                            />
+                                                                            <div className="flex flex-col">
+                                                                                <p className="text-md">
+                                                                                    {participant.User.username || 'Utilisateur inconnu'}
+                                                                                </p>
+                                                                            </div>
+                                                                        </li>
+                                                                    ))}
+                                                                </ul>
+                                                            ) : (
+                                                                <p>Aucun participant disponible.</p>
+                                                            )}
+                                                        </ModalBody>
+                                                        <ModalFooter>
+                                                            <Button color="danger" variant="light" onPress={onClose}>
+                                                                Close
+                                                            </Button>
+                                                        </ModalFooter>
+                                                    </>
+                                                )}
+                                            </ModalContent>
+                                        </Modal>
+                                    </div>
+                                    <div>
+
+                                    <InviteUserForm style={{marginLeft:'10px'}} eventId={event.event_id} />
+
+                                    </div>
+                                </div>
+
+
+                            )}
+                        </div>
+                    </CardBody>
+                    <Divider />
+                    <CardFooter className="flex flex-col gap-4">
+
+                        <div className="flex gap-4 items-center">
+                            <div className="max-w-lg mx-auto p-4">
+                                <h2 className="text-lg font-semibold mb-4">Commentaires</h2>
+                                {event.Comments.length > 0 ? (
+                                    <ul className="list-none p-0">
+                                        {event.Comments.map((comment) => (
+                                            <li key={comment.id} className=" p-4 mb-2 rounded flex items-start">
+                                                <Avatar
+                                                    src={comment.avatar}
+                                                    alt="Avatar"
+                                                    className="flex-shrink-0 mr-4"
+                                                    size="sm"
+                                                />
+                                                <div className="flex-1">
+                                                    <p className="font-medium mb-1">{comment.content || 'Pas de contenu'}</p>
+                                                    <p><RatingOneComment rating={comment.rating} /></p>
+
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <p>Aucun commentaire disponible.</p>
+                                )}
+                            </div>
+
+                        </div>
+
+
+
                         <Button onPress={onCommentOpen}>Ajouter un commentaire</Button>
                         <Modal isOpen={isCommentModalOpen} onOpenChange={onCommentClose}>
                             <ModalContent>
-
                                 <ModalBody>
                                     <div>Notez l'évènement</div>
                                     <div className="flex" onClick={handleAddNote}>{renderStars()}</div>
@@ -307,20 +392,16 @@ const EventDetails = () => {
                                 </ModalFooter>
                             </ModalContent>
                         </Modal>
-                        {currentUser && currentUser.userId === event.created_by && (
-                            <>
 
-                                <Button color="primary" onClick={handleInviteUser}>
-                                    Inviter un utilisateur
-                                </Button>
 
-                            </>
-                        )}
-                    </CardBody>
+                    </CardFooter>
+
                 </Card>
             </div>
         </div>
     );
+
+
 };
 
 export default EventDetails;
